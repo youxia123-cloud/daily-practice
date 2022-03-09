116ffeefa605780d5317acff6491a2bca0037b95
Hi <71977190+youxia123-cloud@users.noreply.github.com>
2022-03-09 21:12:00 +0800

    day3-9

---
+++ python/opencv/chapter1.py
@@ -0,0 +1,35 @@
+import cv2
+
+# 图像导入
+# img = cv2.imread('Resources/car.jpg')
+#
+# cv2.imshow('Output',img)
+# cv2.waitKey(0)
+
+# 视频导入
+
+# cap = cv2.VideoCapture("Resources/sucai.mp4")
+#
+# while True:
+#     success, img = cap.read()
+#     cv2.imshow("Video", img)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+
+# 使用网络摄像头
+
+# cap = cv2.VideoCapture(0)
+# # 定义宽度
+# cap.set(3, 640)
+# # 定义高度
+# cap.set(4, 480)
+# # 设置亮度
+# cap.set(10,100)
+#
+# while True:
+#     success, img = cap.read()
+#     cv2.imshow("Video", img)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+
+
